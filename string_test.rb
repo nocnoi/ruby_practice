ab728e70cd8747489d16023f07e21389285767ec
Nisha Salim <nocturne.noire@gmail.com>
2017-01-22 13:15:26 +0530

    Create strings using %q, %Q, and heredocs

--- string_test.rb
+++ string_test.rb
@@ -1,7 +1,21 @@
 # The \a escape sequence rings a bell or beeps
 puts "Hello \a is there a bell?"
 
-gets ""
-
 # Ruby expressions inside a string
 puts "I'm calculating 5 * 3 here = #{5*3}"
+
+# Another way to create a string. Like using single quotes. The \n below won't create a newline
+puts %q{"This string \n was created using %q"}
+
+# Yet another way of creating a string. This is like using double quotes. \n will create newline
+puts %Q{"This one \n is creating using %Q"}
+
+# String creation using 'here documents' or heredocs
+my_string = <<my_string
+  I am typing something random here
+  It will be formatted exactly as it is typed here
+  Put string inside << and start with delimiter my_string
+  I'll also end with the delimiter after this line.
+my_string
+
+puts my_string
