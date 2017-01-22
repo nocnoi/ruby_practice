ec774feb80385e7e7bf3db4f0990b5a90dfa8984
Nisha Salim <nocturne.noire@gmail.com>
2017-01-22 13:51:45 +0530

    Create file to test numbers

---
+++ number_test.rb
@@ -0,0 +1,13 @@
+
+
+# Base 10
+puts "Base 10: 3 + 4 = #{3 + 4}"
+
+# Base 2
+puts "Base 2: 01 + 10 = #{0b01 + 0b10}"
+
+# Base 16
+puts "Base 16: 0x5C1 = #{0x5C1}"
+
+# Base 8
+puts "Base 8: 01411 = #{01411}"
